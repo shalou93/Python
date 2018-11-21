611840445e78d8c1ec0fff7874626fa8f5384592
刘刚 <441789027@qq.com>
2018-11-21 22:37:30 +0800

    Update infomation.py

--- infomation.py
+++ infomation.py
@@ -14,19 +14,48 @@ def show_info():
     """
     print(info)
 
-def print_info():
+
+
+def open_file():
     with open('E:\Desktop\Py\info.txt','r+') as f:
         data = f.read().split()
+        global p_data
         p_data = data[i].split(',')
-        while True:
-            for x,p in enumerate(p_data):
-                print("%s %s" %(x,p))
-            user_action = input("请输入您要修改的字段值:")
 
+def print_info():
+        open_file()
+        print(p_data)
+
+
+def print_column():
+        global user_column
+        user_column = str(input('请输入修改后的值:'))
 
 
 def change_info():
-    pass
+    open_file()
+    for x, p in enumerate(p_data):
+        print("%s %s" % (x, p))
+    while True:
+        user_action = int(input("请输入您要修改的字段值:"))
+        # try:
+        if user_action == 'q':
+            print('已退出登录!')
+        else:
+            print_column()
+            p_data[user_action] = user_column
+            # p_data.insert(len(data),'\n')
+            p_data.append('\n')
+            print(type((p_data)))
+            print(data[i])
+            data[i] = ','.join(p_data)
+            print(data)
+            print(type(data))
+        # print(data[i].split())
+        with open('E:\Desktop\Py\info.txt', 'r+') as f:
+            f.writelines(data)
+        break
+
 
 while login_count < 3 and run_flag == True:
     user_username = str(input("Username:"))
@@ -40,7 +69,7 @@ while login_count < 3 and run_flag == True:
             if user_username == data[i].split(',')[0] and user_password == data[i].split(',')[1]:
                 print("You are login.")
                 run_flag = False
-                break 
+                break
 
 while run_flag == False:
     show_info()
