c9c6a47a3a0d6d51c0f4941ad1b4dccc273fac4f
刘刚 <441789027@qq.com>
2019-10-18 17:13:41 +0800

    Create prime2.py

---
+++ prime2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# 打印100以内所有的质数
+
+import math
+def func_get_prime(n):
+    return filter(lambda x: not [x%i for i in range(2, int(math.sqrt(x))+1) if x%i ==0], range(2,n+1))
+ 
+print(list(func_get_prime(100)))
