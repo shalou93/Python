b0ef576e0badf2d894dd81c77cc6e65cd656a05a
刘刚 <441789027@qq.com>
2019-10-18 10:56:39 +0800

    Create diamond.py

---
+++ diamond.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# 打印n阶菱形
+
+num = int(input("Please input the num: "))
+
+for i in range(-num,num+1):
+    if i < 0:
+        i = -i
+    else:
+        i = i
+    print(" "*(i) + "*"*(2*(num+1-i)-1) )
