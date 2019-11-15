8e3551d3eead9c6a2cb40ab4249b24b249569e38
刘刚 <441789027@qq.com>
2019-11-16 03:03:39 +0800

    Create reversed.py

---
+++ reversed.py
@@ -0,0 +1,12 @@
+#！/usr/bin/env python
+# 将整数反转输出
+
+num = int(input('num = '))
+reversed_num = 0
+while num > 0:
+    reversed_num = reversed_num * 10 + num % 10
+    num //= 10
+print(reversed_num)
+
+
+print(num[::-1])
