efeac0d05b69f24c970b491851a34af2b8c71d43
刘刚 <441789027@qq.com>
2019-11-16 02:00:38 +0800

    Update prime.py

--- prime.py
+++ prime.py
@@ -3,9 +3,10 @@
 # 打印100以内的所有质数
 # 参考优化算法 http://www.lovebear.top/2017/01/31/GetPrimeList/
 
+from math import sqrt
 l = []
 for i in range(2,100):
-    for j in range(2,i):
+    for j in range(2,int(sqrt(i))+1):
         if i % j == 0:
             break
     else:
