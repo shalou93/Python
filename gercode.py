085bf44e14ec2e75b04434d3d72f4083c2f1af57
刘刚 <441789027@qq.com>
2019-11-20 21:43:43 +0800

    Update gercode.py

--- gercode.py
+++ gercode.py
@@ -2,14 +2,19 @@
 # -*- coding:utf-8 -*-
 # 生成指定位数的随机验证码
 
-
 import string
 import random
 
-all_par = (string.ascii_letters+string.digits)
+all_chr = (string.ascii_letters+string.digits)
 
-def ger_code(len=4):
-    code = "".join(random.sample(all_par,len))
+def ger_code(code_len=4):
+    """
+    生成指定长度的验证码
+    :param code_len: 验证码的长度(默认4个字符)
+    :return: 由大小写英文字母和数字构成的随机验证码
+    """
+    code = "".join(random.sample(all_chr,code_len))
     return code
 
 print(ger_code(4))
+
